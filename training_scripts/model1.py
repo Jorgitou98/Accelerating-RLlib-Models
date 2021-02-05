c62e9b22b12195513fa8cd1ae1cbc18afd3b26f3
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-02-05 13:17:28 +0100

    Update model1.py

--- training_scripts/model1.py
+++ training_scripts/model1.py
@@ -13,7 +13,8 @@ shutil.rmtree('~/ray_results', ignore_errors = True, onerror = False)
 ray.shutdown()
 ray.init()
 config = ppo.DEFAULT_CONFIG.copy()
-config['num_workers'] = 8 
+num_workers = int(sys.argv[1])
+config['num_workers'] = num_workers
 agent = ppo.PPOTrainer(env='Pong-v0')
 policy=agent.get_policy()
 print(policy.model.model_config)
@@ -24,8 +25,8 @@ print("\nConfiguración del modelo del agente:\n\n" + str(config["model"]))
 
 t0 = time.time()
 checkpoint_root='./checkpoints/model1'
-n_iter = int(sys.argv[1])
-save_file = sys.argv[2]
+n_iter = int(sys.argv[2])
+save_file = sys.argv[3]
 training.full_train(checkpoint_root, agent, n_iter, save_file)
 t1 = time.time()-t0
 print("Total time for the " + str(n_iter) + " training iterations: " + str(t1))
