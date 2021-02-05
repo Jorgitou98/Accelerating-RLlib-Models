4fa789daf9e20d451513c73c8c770d8169bbba7e
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-02-05 12:01:29 +0100

    Update model1.py

--- training_scripts/model1.py
+++ training_scripts/model1.py
@@ -26,6 +26,6 @@ t0 = time.time()
 checkpoint_root='./checkpoints/model1'
 n_iter = int(sys.argv[1])
 save_file = sys.argv[2]
-training.full_train(checkpoint_root, agent, n_iter)
+training.full_train(checkpoint_root, agent, n_iter, save_file)
 t1 = time.time()-t0
 print("Total time for the " + str(n_iter) + " training iterations: " + str(t1))
