0dd93f9e967556084d34e2f72b788078b5fe4f40
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-02-22 00:28:24 +0100

    Update model_rollout.py

--- model_rollout.py
+++ model_rollout.py
@@ -9,6 +9,6 @@ output_dir = input("Enter the output file path: ")
 output_pkl = output_dir + '.pkl'
 output_csv = output_dir + '.csv'
 t0 = time.time()
-os.system("python rollout.py " + restore_path + " --env=Pong-v0 --run=PPO --episodes=" + str(num_episodes) + " --out=" + output_pkl + " --time-output=" + output_csv + " --save-info --use-shelve")
+os.system("python rollout.py " + restore_path + " --env=Pong-v0 --run=PPO --episodes=" + str(num_episodes) + " --out=" + output_pkl + " --time-output=" + output_csv + " --save-info --use-shelve --no-render")
 t1 = time.time() - t0
 print("Rollout total time: " + str(t1))
