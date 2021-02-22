ddc5daca398175e037f32d3f492ac7477843d22d
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-02-23 00:03:08 +0100

    Update training_results_analizer.py

--- result_analysis_scripts/training_results_analizer.py
+++ result_analysis_scripts/training_results_analizer.py
@@ -36,9 +36,12 @@ def get_data_models(directory,it_ini, it_fin, policy, model_name, model_name_all
     os.chdir(directory)
     for i in range(1,7):
         model_name = model_name.format(i)
-        model_name_all = model_name_all.format(i)     
+        model_name_all = model_name_all.format(i) 
+        print(model_name) 
+        print(model_name_all)    
         if(len([j for j in glob.glob(model_name)]) != 1):
             model_names_list = [k for k in glob.glob(model_name_all)]
+            print(model_name_list)
             final_model_name_list = []
             for name in model_names_list:
                 splitted_name = name.split('_')
@@ -73,15 +76,15 @@ def get_data_models(directory,it_ini, it_fin, policy, model_name, model_name_all
             writer.writerow(row)  
 
 def get_data(directory, it_ini, it_fin, it_ini_gpu, it_fin_gpu, policy):
-    name_gpu = ('model{}_' + policy+ '_gpu_it_' + str(it_ini) + '_' + str(it_fin))
-    name_no_gpu = name_gpu = ('model{}_' + policy+ '_it_' + str(it_ini) + '_' + str(it_fin))
+    name_gpu = 'model{}_' + policy+ '_gpu_it_' + str(it_ini) + '_' + str(it_fin)
+    name_no_gpu = name_gpu = 'model{}_' + policy + '_it_' + str(it_ini) + '_' + str(it_fin)
     aggregated_results_name_gpu = '~/Mejorando-el-Aprendizaje-Automatico/result_analysis/training_results/results_{}_gpu_it_{}_{}.csv'.format(policy, it_ini_gpu, it_fin_gpu)
     aggregated_results_name_no_gpu = '~/Mejorando-el-Aprendizaje-Automatico/result_analysis/training_results/results_{}_it_{}_{}.csv'.format(policy, it_ini, it_fin)
     model_name_all_gpu = 'model{}_' + policy+ '_gpu_it_*'
     model_name_all_no_gpu = 'model{}_' + policy+ '_it_*'
     name_split_len_gpu = 6
     name_split_len_no_gpu = 5
-    get_data_models(directory,it_ini, it_fin, policy,name_no_gpu, model_name_all_no_gpu, name_split_len_no_gpu, aggregated_results_name_no_gpu)
+    get_data_models(directory,it_ini, it_fin, policy, name_no_gpu, model_name_all_no_gpu, name_split_len_no_gpu, aggregated_results_name_no_gpu)
     get_data_models(directory,it_ini_gpu, it_fin_gpu, policy,name_gpu, model_name_all_gpu, name_split_len_gpu, aggregated_results_name_gpu)
 
 
