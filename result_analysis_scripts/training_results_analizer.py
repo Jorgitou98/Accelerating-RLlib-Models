a1815e49347d1280135f69172e979b3a4ebdeb97
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-02-22 16:16:15 +0100

    Update training_results_analizer.py

--- result_analysis_scripts/training_results_analizer.py
+++ result_analysis_scripts/training_results_analizer.py
@@ -5,7 +5,6 @@ import sys
 import pandas as pd
 
 def merge_csv(files_to_merge, merged_name):
-    print("Estoy dentro")
     combined_csv = pd.concat([pd.read_csv(f) for f in files_to_merge])
     combined_csv.to_csv( merged_name, index=False, encoding='utf-8-sig')
 
@@ -14,21 +13,18 @@ def get_data(directory,it_ini, it_fin, policy):
     name = 'model{}_{}_gpu_it_' + str(it_ini) + '_' + str(it_fin)
     for i in range(1,7):
         model_name = name.format(i, policy)
-        print(model_name)
         
         if(len([j for j in glob.glob(model_name)]) != 1):
             model_names_list = [k for k in glob.glob('model{}_{}_gpu_it_*'.format(i, policy))]
-            print(model_names_list)
             final_model_name_list = []
             for name in model_names_list:
                 splitted_name = name.split('_')
                 if(len(splitted_name) == 6 and int(splitted_name[4]) >= it_ini and int(splitted_name[4]) <= it_fin and int(splitted_name[5]) <= it_fin and int(splitted_name[5]) >= it_ini):
                     final_model_name_list.append(name + '/progress.csv')
-            print( directory + '/' + model_name + '/progress.csv')
+            os.mkdir(directory + '/' + model_name)
             merge_csv(final_model_name_list, directory + '/' + model_name + '/progress.csv')
 
 def main():
-    print("Hola")
     directory = sys.argv[1]
     it_ini = int(sys.argv[2])
     it_fin = int(sys.argv[3])
