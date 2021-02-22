2715b262a709b62840009a060eacec0b43c370a8
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-02-23 00:29:59 +0100

    Update training_results_analizer.py

--- result_analysis_scripts/training_results_analizer.py
+++ result_analysis_scripts/training_results_analizer.py
@@ -68,7 +68,7 @@ def get_data_models(directory,it_ini, it_fin, policy, model_name_format, model_n
         aggregated_data_this_model['mean_update_time_ms'] = df['timers/update_time_ms'].mean()
         aggregated_results.append(aggregated_data_this_model)
 
-    chdir(save_directory)
+    os.chdir(save_directory)
     with open(aggregated_results_name, mode='w+') as csv_agg_file:
         fieldnames = list(aggregated_results[0].keys())
         writer = csv.DictWriter(csv_agg_file, fieldnames=fieldnames)
