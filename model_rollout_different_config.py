f4081356cfaabd0dff42b9033328388947cf9470
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-03-05 12:20:24 +0100

    Update model_rollout_different_config.py

--- model_rollout_different_config.py
+++ model_rollout_different_config.py
@@ -11,6 +11,6 @@ config = sys.argv[4]
 output_pkl = output_dir + '.pkl'
 output_csv = output_dir + '.csv'
 t0 = time.time()
-os.system("python rollout.py " + restore_path + " --env=Pong-v0 --run=PPO --episodes='" + str(num_episodes) + " --out=" + output_pkl + " --time-output=" + output_csv + " --save-info --use-shelve --no-render --config=" + config + "'")
+os.system("python rollout.py " + restore_path + " --env=Pong-v0 --run=PPO --episodes=" + str(num_episodes) + " --out=" + output_pkl + " --time-output=" + output_csv + " --save-info --use-shelve --no-render --config=" + config + "'")
 t1 = time.time() - t0
 print("Rollout total time: " + str(t1))
