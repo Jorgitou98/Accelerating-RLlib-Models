9cd5e2303b4e9250a9921b0a683218403e26ae8c
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-03-16 11:29:38 +0100

    Update training_results_combiner.py

--- result_analysis_scripts/training_results_combiner.py
+++ result_analysis_scripts/training_results_combiner.py
@@ -8,7 +8,7 @@ def combine_data(directory,it_ini, it_fin):
     os.chdir(directory)
     name = "model{}_ppo_gpu_it_{}_{}/progress.csv"
     for model in [1,3,4]:
-        for i in range(it_ini, it_fin+1, 1000):
+        for i in range(it_ini, it_fin, 1000):
             file_names.append(name.format(model, i+1, i+1000))
         combined_name = "../ray_results_gathered/model{}_ppo_gpu_it_{}_{}.csv".format(model, it_ini+1, it_fin)
         training_results_analizer.merge_csv(file_names, combined_name)
