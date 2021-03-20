d7488fd18e6b27c974c00da41e23d408676e7fbf
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-03-20 01:13:39 +0100

    train results organizados

--- result_analysis_scripts/training_results_combiner.py
+++ result_analysis_scripts/training_results_combiner.py
@@ -5,21 +5,21 @@ import pandas as pd
 import plot_results
 
 
-def combine_data(directory,it_ini, it_fin):
+def combine_data(directory,it_ini, it_fin, model_ids):
     os.chdir(directory)
     name = "model{}_ppo_gpu_it_{}_{}/progress.csv"
-    for model in [1,3,4]:
+    for model in model_ids:
         file_names = []
         for i in range(it_ini, it_fin, 1000):
             file_names.append(name.format(model, i+1, i+1000))
-        combined_name = "../ray_results_gathered/model{}_ppo_gpu_it_{}_{}.csv".format(model, it_ini+1, it_fin)
+        combined_name = "../ray_results_gathered/{}_iters/model{}_ppo_gpu_it_{}_{}.csv".format(it_fin, model, it_ini+1, it_fin)
         training_results_analizer.merge_csv(file_names, combined_name)
 
-def plot_data(it_ini, it_fin):
-    os.chdir('ray_results_gathered')
+def plot_data(it_ini, it_fin, model_ids):
+    os.chdir('ray_results_gathered/{}_iters'.format(it_fin))
     file_names = ['model{}_ppo_gpu_it_{}_{}.csv'.format(i,it_ini+1,it_fin) for i in[1,3,4]]
     df_list = []
-    for i in range(0,3):
+    for i in range(0,len(model_ids)):
         df_list.append(pd.read_csv(file_names[i]))
 
     #Compare the three models results
@@ -38,38 +38,41 @@ def plot_data(it_ini, it_fin):
     x_values = [i for i in range(it_ini+1, it_fin+1)]
     for var in vars_to_compare:
         y_values = []
-        for i in range(0,3):
+        for i in range(0,len(model_ids)):
             y_values.append(list(df_list[i][var]))
-        labels = ['model1', 'model3', 'model4']
-        title = 'Compare ' + var + ' models 1,3 and 4'
+        labels = ['model{}'.format(i) for i in model_ids]
+        models_str_title=' models'
+        models_str_save ='_models'
+        for i in model_ids:
+            models_str_title = models_str_title + ' ' + i
+            models_str_save = models_str_save + '_' + i
+
+        title = 'Compare ' + var + models_str_title
         var_save_name = var.split('/')[len(var.split('/'))-1]
-        save_name = 'graphs/compare_' + var_save_name + '_it_' + str(it_ini + 1) + '_' + str(it_fin) +'_models_1_3_4.png'
+        save_name = 'graphs/compare_' + var_save_name + '_it_' + str(it_ini + 1) + '_' + str(it_fin) + models_str_save + '.png'
         plot_results.plot_line_three(x_values, y_values, labels, title, save_name)
 
     # See rewards evolution for each model
-    models=[1,3,4]
-    for i in range(0,3):
+    for i in range(0,len(model_ids)):
         y_values = []
         for var in ['episode_reward_max','episode_reward_min','episode_reward_mean']:
             y_values.append(list(df_list[i][var]))
         labels = ['max reward','min reward','average reward']
-        title = 'Rewards Model {} iterations {} to {}'.format(models[i], it_ini+1, it_fin)
-        save_name = 'graphs/rewards_model_{}_it_{}_{}.png'.format(models[i], it_ini+1, it_fin)
+        title = 'Rewards Model {} iterations {} to {}'.format(model_ids[i], it_ini+1, it_fin)
+        save_name = 'graphs/rewards_model_{}_it_{}_{}.png'.format(model_ids[i], it_ini+1, it_fin)
         plot_results.plot_line_three(x_values, y_values, labels, title, save_name)
 
 
 
 
 def main():
-    '''
     directory = sys.argv[1]
     it_ini = int(sys.argv[2])
     it_fin = int(sys.argv[3])
-    combine_data(directory,it_ini,it_fin)
-    '''
-    it_ini = int(sys.argv[1])
-    it_fin = int(sys.argv[2])
-    plot_data(it_ini, it_fin)
+    model_ids_str = sys.argv[4]
+    model_ids = model_ids_str[1:len(model_ids_str)-1].split(',')
+    combine_data(directory,it_ini,it_fin, model_ids)
+    plot_data(it_ini, it_fin, model_ids)
     
 
 if __name__ == '__main__':
