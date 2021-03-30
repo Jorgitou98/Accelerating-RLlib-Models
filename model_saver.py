5daad7b39e23579c8c1b79bd38feec8674281c3c
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-03-30 20:53:37 +0200

    Create model_saver.py

---
+++ model_saver.py
@@ -0,0 +1,19 @@
+import ray
+import ray.cloudpickle as cloudpickle
+import tensorflow as tf
+import ray.rllib.agents.ppo as ppo
+
+checkpoint_dir=sys.argv[1]
+export_name = sys.argv[2]
+ray.shutdown()
+ray.init()
+config_dir = os.path.dirname(checkpoint_dir)
+config_path = os.path.join(config_dir, "params.pkl")
+with open(config_path, "rb") as f:
+    config = cloudpickle.load(f)
+print(config)
+agent = ppo.PPOTrainer(config = config, env='Pong-v0')
+agent.restore(checkpoint_dir)
+agent.export_policy_model(export_name)
+ray.shutdown()
+
