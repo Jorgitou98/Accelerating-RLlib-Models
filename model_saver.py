ab931067c1c47cfaf6622479a578bc72f7f3be9e
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-03-30 20:59:36 +0200

    Update model_saver.py

--- model_saver.py
+++ model_saver.py
@@ -2,6 +2,7 @@ import ray
 import ray.cloudpickle as cloudpickle
 import tensorflow as tf
 import ray.rllib.agents.ppo as ppo
+import sys
 
 checkpoint_dir=sys.argv[1]
 export_name = sys.argv[2]
@@ -9,6 +10,8 @@ ray.shutdown()
 ray.init()
 config_dir = os.path.dirname(checkpoint_dir)
 config_path = os.path.join(config_dir, "params.pkl")
+if not os.path.exists(config_path):
+    config_path = os.path.join(config_dir, "../params.pkl")
 with open(config_path, "rb") as f:
     config = cloudpickle.load(f)
 print(config)
