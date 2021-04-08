d1720584d3a3422d7da2c0de1181bcf8b50021ff
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-08 23:42:34 +0200

    Update model_saver.py

--- model_saver.py
+++ model_saver.py
@@ -12,7 +12,7 @@ checkpoint_dir=sys.argv[1]
 export_name = sys.argv[2]
 config = ppo.DEFAULT_CONFIG.copy()
 print(config)
-'''
+
 config_dir = os.path.dirname(checkpoint_dir)
 config_path = os.path.join(config_dir, "params.pkl")
 if not os.path.exists(config_path):
@@ -20,7 +20,7 @@ if not os.path.exists(config_path):
 with open(config_path, "rb") as f:
     config = cloudpickle.load(f)
 print(config)
-'''
+
 agent = ppo.PPOTrainer(config, env='Pong-v0')
 agent.restore(checkpoint_dir)
 agent.export_policy_model(export_name)
