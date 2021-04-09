bb78af731f26d7e92764e69c919d5420b5b34d10
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-10 00:32:54 +0200

    Update model_saver.py

--- model_saver.py
+++ model_saver.py
@@ -13,7 +13,7 @@ export_name = sys.argv[2]
 config = ppo.DEFAULT_CONFIG.copy()
 print(config)
 
-'''
+
 config_dir = os.path.dirname(checkpoint_dir)
 config_path = os.path.join(config_dir, "params.pkl")
 if not os.path.exists(config_path):
@@ -21,7 +21,8 @@ if not os.path.exists(config_path):
 with open(config_path, "rb") as f:
     config = cloudpickle.load(f)
 print(config)
-'''
+config['num_gpus']=0
+config['num_gpus_per_worker'] = 0
 
 agent = ppo.PPOTrainer(config, env='Pong-v0')
 agent.restore(checkpoint_dir)
