ecf3a646afcfdccdcb40383a8ca7145c20e8a5b2
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-11 16:28:50 +0200

    Update train_model_ppo_no_cpu_volta1.py

--- training_scripts/train_model_ppo_no_cpu_volta1.py
+++ training_scripts/train_model_ppo_no_cpu_volta1.py
@@ -94,6 +94,6 @@ print("Configuracion del agente:\n\n" + str(config))
 print("\nConfiguracion del modelo del agente:\n\n" + str(config["model"]))
 
 t0 = time.time()
-training.full_train(checkpoint_root, agent, n_iter, save_file)
+training.full_train(checkpoint_root, agent, n_iter, save_file).remote()
 t1 = time.time()-t0
 print("Total time for the " + str(n_iter) + " training iterations: " + str(t1))
