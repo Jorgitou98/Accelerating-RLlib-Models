d2b871dbb7f742cbca6725bc8da60782b4393962
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-13 12:14:48 +0200

    Update training_results_analizer_volta1.py

--- result_analysis_scripts/training_results_analizer_volta1.py
+++ result_analysis_scripts/training_results_analizer_volta1.py
@@ -58,7 +58,7 @@ def get_data(directory, model_names, model_names_short, model, it_ini, it_fin):
     aggregated_results = get_data_models(directory, model_names, model_names_short, aggregated_results_name, save_directory)
     
     vars = ['mean_time_this_iter_s','mean_sample_time_ms','mean_sample_throughput','mean_load_time_ms','mean_load_throughput','mean_learn_time_ms','mean_learn_throughput','mean_update_time_ms', 'mean_ram_util_percent', 'mean_cpu_util_percent']
-    y_labels['time(s)', 'time(ms)', None, 'time(ms)', None, 'time(ms)', None, 'time(ms)', '% util', '% util']
+    y_labels=['time(s)', 'time(ms)', None, 'time(ms)', None, 'time(ms)', None, 'time(ms)', '% util', '% util']
     
     for i in range(len(vars)):
         var_values = [aggregated_results[i][vars[i]] for i in range(0,len(model_names))]
