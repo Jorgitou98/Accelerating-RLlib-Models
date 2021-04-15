ada209fa12200e4e81f8b76b12fafba59ac8be67
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-15 16:48:24 +0200

    changes rollout analysis

--- result_analysis_scripts/rollout_results_analizer_volta1.py
+++ result_analysis_scripts/rollout_results_analizer_volta1.py
@@ -40,20 +40,20 @@ def get_data_models(name, aggregated_results_name, trained_model_descr, model_id
     return aggregated_results
 
 def get_data(trained_model_descr, model_ids, directory, num_iters):
-    descriptions = ['8_workers_gpu0', '8_workers_gpu1', '8_workers_both_gpus', '8_workers_no_gpus']
+    descriptions = ['8_workers_gpu0', '8_workers_gpu1', '8_workers_both_gpus', '16_workers_both_gpus', '0_workers_gpu0', '0_workers_gpu1', '0_workers_both_gpus', '0_workers_gpu0_one_cpu', '0_workers_gpu1_one_cpu', '0_workers_both_gpus_one_cpu', '0_workers_no_gpus']
     dir = dirname(dirname(abspath(__file__)))
-    save_directory = dir + '/result_analysis/rollout_results/volta1_updated_{}_iters'.format(num_iters)
+    save_directory = dir + '/result_analysis/rollout_results/volta1_{}_iters'.format(num_iters)
     if not os.path.exists(save_directory):
         os.mkdir(save_directory)
     names = ['trained_{}/model{}_' + i + '.csv' for i in descriptions]
-    aggregated_names = ['results_volta1_updated_{}_iters_{}.csv'.format(num_iters, i) for i in descriptions]
+    aggregated_names = ['results_volta1_{}_iters_{}.csv'.format(num_iters, i) for i in descriptions]
     aggregated_results = [get_data_models(names[i], aggregated_names[i], trained_model_descr, model_ids, directory, num_iters, save_directory) for i in range(0, len(descriptions))]
 
     model_names = ['model {}'.format(i) for i in model_ids]
     vars = ['average_model_time_per_episode', 'average_steps_per_episode', 'average_model_time_per_step', 'average_reward_per_episode', 'min_reward', 'max_reward']
 
-    if not os.path.exists(dir + '/result_analysis/rollout_results/volta1_updated_{}_iters/graphs/'.format(num_iters)):
-        os.mkdir(dir + '/result_analysis/rollout_results/volta1_updated_{}_iters/graphs/'.format(num_iters))
+    if not os.path.exists(dir + '/result_analysis/rollout_results/volta1_{}_iters/graphs/'.format(num_iters)):
+        os.mkdir(dir + '/result_analysis/rollout_results/volta1_{}_iters/graphs/'.format(num_iters))
 
     for var in vars:
         var_values_list = []
@@ -61,12 +61,12 @@ def get_data(trained_model_descr, model_ids, directory, num_iters):
             var_values=[aggregated_results[desc][i][var] for i in range(0, len(model_ids))]
             var_values_list.append(var_values)
             title = var + ' per model {} {} iters'.format(descriptions[desc], num_iters)
-            save_name= dir + '/result_analysis/rollout_results/volta1_updated_'+ str(num_iters) + '_iters/graphs/' + var + '_per_model_' + descriptions[desc] +'_' +str(num_iters) + '_iters.png'
+            save_name= dir + '/result_analysis/rollout_results/volta1_'+ str(num_iters) + '_iters/graphs/' + var + '_per_model_' + descriptions[desc] +'_' +str(num_iters) + '_iters.png'
             plot_results.plot_bars(model_names, var_values, title, save_name)
 
 
         title_combined = var + ' per model all configurations'
-        save_name_combined = dir + '/result_analysis/rollout_results/volta1_updated_' + str(num_iters) + '_iters/graphs/'+ var + '_per_model_all_descr.png' 
+        save_name_combined = dir + '/result_analysis/rollout_results/volta1_' + str(num_iters) + '_iters/graphs/'+ var + '_per_model_all_descr.png' 
         plot_results.plot_bars_multiple(model_names, var_values_list, title_combined, save_name_combined, ['rollout {}'.format(desc) for desc in descriptions])
 
     for i in range(0, len(descriptions)-1):
@@ -82,10 +82,10 @@ def get_data(trained_model_descr, model_ids, directory, num_iters):
             for var in vars:
                 var_values_speedup = [speedups[m][var] for m in range(0,len(model_ids))]
                 title = var + ' speedup {} vs {}'.format(descriptions[i], descriptions[j])
-                save_name = dir + '/result_analysis/rollout_results/volta1_updated_' + str(num_iters) + '_iters/graphs/'+ var + '_speedup_' + descriptions[i] + '_vs_'+ descriptions[j] + '_' + str(num_iters) + '_iters.png'
+                save_name = dir + '/result_analysis/rollout_results/volta1_' + str(num_iters) + '_iters/graphs/'+ var + '_speedup_' + descriptions[i] + '_vs_'+ descriptions[j] + '_' + str(num_iters) + '_iters.png'
                 plot_results.plot_bars(model_names, var_values_speedup, title, save_name)
  
-            with open(dir + '/result_analysis/rollout_results/volta1_updated_{}_iters/results_speedup_{}_vs_{}_{}_iters.csv'.format(num_iters,descriptions[i], descriptions[j], num_iters), mode='w+') as csv_speedup_file:
+            with open(dir + '/result_analysis/rollout_results/volta1_{}_iters/results_speedup_{}_vs_{}_{}_iters.csv'.format(num_iters,descriptions[i], descriptions[j], num_iters), mode='w+') as csv_speedup_file:
                 fieldnames = list(speedups[0].keys())
                 writer = csv.DictWriter(csv_speedup_file, fieldnames=fieldnames)
                 writer.writeheader()
