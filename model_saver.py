01b8a27dac0d5516281ec791012a68afa20eba31
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-20 19:46:46 +0200

    Update model_saver.py

--- model_saver.py
+++ model_saver.py
@@ -33,12 +33,12 @@ config['num_gpus_per_worker'] = 0
 agent = ppo.PPOTrainer(config, env='Pong-v0')
 agent.restore(checkpoint_dir)
 
-#tf.compat.v1.enable_eager_execution()
+
 with agent.get_policy().get_session().graph.as_default():
     export_model = agent.get_policy().model.base_model.save(export_name + '.h5')
 
 
-
+tf.compat.v1.enable_eager_execution()
 converter = tf.lite.TFLiteConverter.from_keras_model(agent.get_policy().model.base_model)
 model = converter.convert()
 
