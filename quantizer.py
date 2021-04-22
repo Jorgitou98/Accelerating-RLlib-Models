d6ffbb8a3341ef4085865603364b091fcf13c432
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-22 16:58:27 +0200

    Update quantizer.py

--- quantizer.py
+++ quantizer.py
@@ -16,7 +16,7 @@ model = tf.keras.models.load_model(h5_dir, custom_objects={'tf':tf})
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.representative_dataset = representative_data_gen
-onverter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
 converter.inference_input_type = tf.uint8
 converter.inference_output_type = tf.uint8
 tflite_quant_model = converter.convert()
