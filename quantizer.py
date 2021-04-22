eb8d59d07ad8ad0012cc5cd042e6b22a742dc8a7
Javier Guzmán Muñoz <javiguzman2009@hotmail.com>
2021-04-22 17:01:19 +0200

    Update quantizer.py

--- quantizer.py
+++ quantizer.py
@@ -20,3 +20,9 @@ converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
 converter.inference_input_type = tf.uint8
 converter.inference_output_type = tf.uint8
 tflite_quant_model = converter.convert()
+
+interpreter = tf.lite.Interpreter(model_content=tflite_model_quant)
+input_type = interpreter.get_input_details()[0]['dtype']
+print('input: ', input_type)
+output_type = interpreter.get_output_details()[0]['dtype']
+print('output: ', output_type)
